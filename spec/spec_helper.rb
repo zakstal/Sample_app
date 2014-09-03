e4f4aea71ae8f9c9c4be9936d784dd5bf674aa85
Zak <zak_stallings@zaks-mbp.home>
2014-09-03 18:49:15 -0400

    Finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,26 @@ require 'spork'
 #require 'spork/ext/ruby-debug'
 
 Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However,
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment",__FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| requrie f}
+
+  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+  RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    config.use_transactional_fixtures = true
+
+    config.infer_base_class_for_anonymous_controllers = false
+
+    config.order = "random"
+
+    config.include Capybara::DSL
+  end
 
 end
 
